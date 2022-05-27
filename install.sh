0f97f3b17ee86d7d22891893737642c284285a67
iLkuRu <106349676+iLkuRu@users.noreply.github.com>
2022-05-27 12:17:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ systemctl start sshd
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y &&\
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys &&\
 #docker
+systemctl enable docker
+systemctl start docker
+systemctl enable containerd
+systemctl start containerd
 
 #zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
