985252dfa256099b0986f5197df6e399e08e81ce
iLkuRu <106349676+iLkuRu@users.noreply.github.com>
2022-05-27 13:25:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #update&install
-pacman -Syyu openssh git docker zsh zsh-completions curl systemd-resolvconf netctl --noconfirm
+pacman -Syyu openssh git docker zsh zsh-completions curl --noconfirm
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 #SSH
 systemctl enable sshd
